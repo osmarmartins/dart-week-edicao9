02af00915c75ae8d120c255d8b05ede095595d80
osmar-avell <osmarfilho6@gmail.com>
2023-02-01 11:34:39 -0300

    Aula-2 HomaPage e mixins

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,19 +1,44 @@
+import 'package:dw9_delivery/app/core/ui/helpers/messages.dart';
 import 'package:flutter/material.dart';
+import 'package:dw9_delivery/app/core/ui/helpers/loader.dart';
+import 'package:dw9_delivery/app/core/ui/widgets/delivery_app_bar.dart';
+import 'package:dw9_delivery/app/models/product_model.dart';
+import 'package:dw9_delivery/app/pages/home/widgets/delivery_product_tile.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
+      appBar: DeliveryAppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return DeliveryProductTile(
+                  product: ProductModel(
+                    id: 1,
+                    name: 'X-Salada',
+                    description:
+                        'Lanche acompanha pão, hambúguer, mussarela, alface, tomate e maionese',
+                    price: 15.0,
+                    image:
+                        'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800',
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
-      body: const Center(
-          child: Text(
-        'HomePage',
-        style: TextStyle(fontSize: 24),
-      )),
     );
   }
 }
---
+++ lib/app/pages/home/widgets/delivery_product_tile.dart
@@ -0,0 +1,63 @@
+import 'package:dw9_delivery/app/core/extensions/formatter_extensios.dart';
+import 'package:dw9_delivery/app/core/ui/styles/colors_app.dart';
+import 'package:dw9_delivery/app/core/ui/styles/text_styles.dart';
+import 'package:dw9_delivery/app/models/product_model.dart';
+import 'package:flutter/material.dart';
+
+class DeliveryProductTile extends StatelessWidget {
+  final ProductModel product;
+
+  const DeliveryProductTile({super.key, required this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    product.name,
+                    style:
+                        context.textStyles.textRegular.copyWith(fontSize: 12),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: Text(
+                    product.description,
+                    style: context.textStyles.textLight.copyWith(fontSize: 12),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 12),
+                  child: Text(
+                    product.price.currencyPTBR,
+                    style: context.textStyles.textMedium.copyWith(
+                      fontSize: 11,
+                      color: context.colors.secondary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Column(children: [
+            FadeInImage.assetNetwork(
+              placeholder: 'assets/images/loading.gif',
+              image: product.image,
+              width: 100,
+              height: 100,
+              fit: BoxFit.contain,
+            ),
+          ]),
+        ],
+      ),
+    );
+  }
+}
